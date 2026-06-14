a795114f6473f0e74774c27de16245da21484c81
ahmedshah <cyberahmedshah@gmail.com>
2026-06-14 11:30:30 +0500

    testing compiler

--- Autofile-scripts/auto.py
+++ Autofile-scripts/auto.py
@@ -2,17 +2,17 @@ import os
 import shutil
 
 
-# os.makedirs("Projects", exist_ok=True)
+os.makedirs("Projects", exist_ok=True)
 
-# with open("Projects/char.txt", "w") as f:
-#     pass 
+with open("Projects/char.txt", "w") as f:
+     pass 
 
 
 shutil.rmtree("Masterclasses")
 
 
-# for i in range (1, 101):
-#     with open (f"Masterclasses/Day{i}", "w") as f:
-#         f.write(f"Welcome to the Day {i} of the masterclass")
+for i in range (1, 101):
+     with open (f"Masterclasses/Day{i}", "w") as f:
+         f.write(f"Welcome to the Day {i} of the masterclass")
 
  
